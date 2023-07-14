4b02e4d1ca26c7a0ec1c2da8283ed84e4519199b
s.starikov <s.starikov@gaskar.group>
2023-07-14 23:14:14 +0500

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Statikov Svyatoslav Sergeevich");
+        student.setGroup("Java");
+        student.setDateAdmission(LocalDate.of(2023, 2, 24));
+        System.out.printf("Student with full name: %s, group: %s, date admission: %s%n", student.getFullName(), student.getGroup(), student.getDateAdmission());
+    }
+}
