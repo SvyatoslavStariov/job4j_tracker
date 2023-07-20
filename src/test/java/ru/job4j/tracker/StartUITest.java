e231d5955408e29956d5423de46ffcf592797714
s.starikov <s.starikov@gaskar.group>
2023-07-20 11:35:12 +0500

    9. Тестирование. Подготовка данных. [#182960 #430822]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -22,8 +22,7 @@ public class StartUITest {
         Item item = new Item("new item");
         tracker.add(item);
         String[] answers = {
-                String.valueOf(item.getId()),
-                "edited item"
+                String.valueOf(item.getId())
         };
         StartUI.editItem(new StubInput(answers), tracker);
         Item edited = tracker.findById(item.getId());
