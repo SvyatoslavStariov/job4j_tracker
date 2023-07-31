748689b54067cf33052a17efab9d8f809958fcc6
s.starikov <s.starikov@gaskar.group>
2023-07-31 19:51:17 +0500

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,37 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+
+    private static Tracker tracker = new Tracker();
+
+    public SingleTracker() {
+    }
+
+    public static Tracker getInstance() {
+        return tracker;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+}
