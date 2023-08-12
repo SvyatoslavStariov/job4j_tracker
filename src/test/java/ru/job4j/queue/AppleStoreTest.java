e8e9faa5d3367dffd4b2b9ee6d3c62a53e41e45e
s.starikov <s.starikov@gaskar.group>
2023-08-12 14:44:43 +0500

    1. Интерфейс Queue [#504942 #436112]

--- src/test/java/ru/job4j/queue/AppleStoreTest.java
+++ src/test/java/ru/job4j/queue/AppleStoreTest.java
@@ -39,4 +39,36 @@ class AppleStoreTest {
         String customer = appleStore.getFirstUpsetCustomer();
         assertThat(customer).isEqualTo("Iryna");
     }
+
+    @Test
+    void whenGetFirstIsEmpty() {
+        Queue<Customer> customers = new LinkedList<>();
+        customers.add(new Customer("Petr", 1000));
+        customers.add(new Customer("Stas", 1500));
+        customers.add(new Customer("Andrey", 850));
+        customers.add(new Customer("Alexei", 900));
+        customers.add(new Customer("Iryna", 1250));
+        customers.add(new Customer("Elena", 750));
+        customers.add(new Customer("Rail", 950));
+        int count = 8;
+        AppleStore appleStore = new AppleStore(customers, count);
+        String customer = appleStore.getFirstUpsetCustomer();
+        assertThat(customer).isEqualTo("");
+    }
+
+    @Test
+    void whenGetLastIsEmpty() {
+        Queue<Customer> customers = new LinkedList<>();
+        customers.add(new Customer("Petr", 1000));
+        customers.add(new Customer("Stas", 1500));
+        customers.add(new Customer("Andrey", 850));
+        customers.add(new Customer("Alexei", 900));
+        customers.add(new Customer("Iryna", 1250));
+        customers.add(new Customer("Elena", 750));
+        customers.add(new Customer("Rail", 950));
+        int count = 8;
+        AppleStore appleStore = new AppleStore(customers, count);
+        String customer = appleStore.getLastHappyCustomer();
+        assertThat(customer).isEqualTo("");
+    }
 }
