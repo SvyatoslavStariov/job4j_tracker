d35eb3343327c16590cf06322c00382f49575b39
s.starikov <s.starikov@gaskar.group>
2023-08-17 11:03:24 +0500

    3. Компаратор для строк [#35008 #437168]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -3,7 +3,7 @@ package ru.job4j.collection;
 public class StringCompare {
     public int compare(String s1, String s2) {
         int size = Math.min(s1.length(), s2.length());
-        for (int i = 0; i < size - 1; i++) {
+        for (int i = 0; i < size; i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
                 return Character.compare(s1.charAt(i), s2.charAt(i));
             }
