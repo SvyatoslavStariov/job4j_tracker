5c53ab9f19adbfaeef1b3eda1d13b40a91553f03
s.starikov <s.starikov@gaskar.group>
2023-08-30 10:59:29 +0500

    3. Отсортировать департаменты [#34352 #440713]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -9,17 +9,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String left, String right) {
         String[] splitLeft = left.split(REGEX);
         String[] splitRight = right.split(REGEX);
-        if (!splitLeft[0].equals(splitRight[0])) {
-            return Character.compare(splitRight[0].charAt(splitRight[0].length() - 1), splitLeft[0].charAt(splitLeft[0].length() - 1));
-        }
-        int size = Math.min(splitLeft.length, splitRight.length);
-        for (int i = 0; i < size; i++) {
-            if (!splitLeft[i].equals(splitRight[i])) {
-                if (splitLeft[i].charAt(splitLeft[i].length() - 1) != splitRight[i].charAt(splitRight[i].length() - 1)) {
-                    return Character.compare(splitLeft[i].charAt(splitLeft[i].length() - 1), splitRight[i].charAt(splitRight[i].length() - 1));
-                }
-            }
-        }
-        return Integer.compare(left.length(), right.length());
+        int result = splitRight[0].compareTo(splitLeft[0]);
+        return result != 0 ? result : left.compareTo(right);
     }
 }
